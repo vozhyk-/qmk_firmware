2aaf55b94931a10e6ed000e79707737da8fd7bdb
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2020-06-15 23:55:30 +0200

    Rename RAISE to SYM, add right-hand NAV

--- keyboards/ergodash/rev1/keymaps/vozhyk-/keymap.c
+++ keyboards/ergodash/rev1/keymaps/vozhyk-/keymap.c
@@ -5,13 +5,13 @@
 
 #define _QWERTY 0
 #define _NAV 1
-#define _RAISE 2
+#define _SYM 2
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   NAV,
-  RAISE,
+  SYM,
   ADJUST,
 };
 
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_RBRC,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSLS,         KC_ESC , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
-    KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_INS,          KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_NO,   KC_NO,            KC_LALT, NAV,     KC_SPC ,         KC_ENT , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LCTL, KC_NO,   KC_NO,            KC_LALT, NAV,     KC_SPC ,         KC_ENT , NAV,     SYM,              KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   [_NAV] = LAYOUT(
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,          _______, _______, _______,         _______, _______, _______,          _______, _______, _______  \
   ),
 
-  [_RAISE] = LAYOUT(
+  [_SYM] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,         KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
@@ -76,20 +76,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case NAV:
       if (record->event.pressed) {
         layer_on(_NAV);
-        update_tri_layer(_NAV, _RAISE, _ADJUST);
+        update_tri_layer(_NAV, _SYM, _ADJUST);
       } else {
         layer_off(_NAV);
-        update_tri_layer(_NAV, _RAISE, _ADJUST);
+        update_tri_layer(_NAV, _SYM, _ADJUST);
       }
       return false;
       break;
-    case RAISE:
+    case SYM:
       if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_NAV, _RAISE, _ADJUST);
+        layer_on(_SYM);
+        update_tri_layer(_NAV, _SYM, _ADJUST);
       } else {
-        layer_off(_RAISE);
-        update_tri_layer(_NAV, _RAISE, _ADJUST);
+        layer_off(_SYM);
+        update_tri_layer(_NAV, _SYM, _ADJUST);
       }
       return false;
       break;
