943ffab7a3fcd25925fa5ffc9656a2fab185e64f
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2020-06-15 18:46:09 +0200

    Add a navigation layer

--- keyboards/ergodash/rev1/keymaps/vozhyk-/keymap.c
+++ keyboards/ergodash/rev1/keymaps/vozhyk-/keymap.c
@@ -4,13 +4,13 @@
 #define LAYOUT LAYOUT_2key
 
 #define _QWERTY 0
-#define _LOWER 1
+#define _NAV 1
 #define _RAISE 2
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  LOWER,
+  NAV,
   RAISE,
   ADJUST,
 };
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL ,         KC_ESC , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LGUI, S_TAP_a, KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, C_TAP_z, KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_NO,   KC_NO,            KC_LALT, LOWER,   KC_SPC ,         KC_ENT , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LCTL, KC_NO,   KC_NO,            KC_LALT, NAV,     KC_SPC ,         KC_ENT , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
-  [_LOWER] = LAYOUT(
+  [_NAV] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,         KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,         KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                              KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
+    _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______, KC_UP,   _______, _______, _______, \
+    _______, _______, _______, KC_BSPC, KC_DEL,  _______, _______,         _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                           KC_END,  KC_ENT,  _______, _______, _______, _______, \
     _______, _______, _______,          _______, _______, _______,         _______, _______, _______,          _______, _______, _______  \
   ),
 
@@ -71,23 +71,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case LOWER:
+    case NAV:
       if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_on(_NAV);
+        update_tri_layer(_NAV, _RAISE, _ADJUST);
       } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_off(_NAV);
+        update_tri_layer(_NAV, _RAISE, _ADJUST);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_NAV, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_NAV, _RAISE, _ADJUST);
       }
       return false;
       break;
