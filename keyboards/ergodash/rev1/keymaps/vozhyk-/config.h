ff3c15991d7507ce83f3af9efc86dac835ddb553
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2020-06-18 19:09:39 +0200

    Lower the tapping term
    Make it easier to press Shift quickly.

--- keyboards/ergodash/rev1/keymaps/vozhyk-/config.h
+++ keyboards/ergodash/rev1/keymaps/vozhyk-/config.h
@@ -34,6 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#define TAPPING_TERM 160
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
