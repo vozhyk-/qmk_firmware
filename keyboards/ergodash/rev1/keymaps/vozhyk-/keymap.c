51a61b1ea368857fd95c7d26797283955d152c9d
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2020-06-18 22:21:16 +0200

    Add a FLIP layer for a right-hand NAV button

--- keyboards/ergodash/rev1/keymaps/vozhyk-/keymap.c
+++ keyboards/ergodash/rev1/keymaps/vozhyk-/keymap.c
@@ -3,12 +3,16 @@
 #undef LAYOUT
 #define LAYOUT LAYOUT_2key
 
-#define _QWERTY 0
-#define _NAV 1
-#define _MOARNAV 2
-#define _SYM 3
+enum layers {
+    _QWERTY,
+    _FLIP,
+    _NAV,
+    _MOARNAV,
+    _SYM
+};
 
 #define NAV MO(_NAV)
+#define FLIP MO(_FLIP)
 #define MOARNAV MO(_MOARNAV)
 #define SYM MO(_SYM)
 
@@ -28,7 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSLS,         KC_ESC , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
     KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_INS,          KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, S_TAP_z, KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LCTL, _______,          KC_LALT, NAV,     KC_SPC ,         KC_ENT,  SYM,     KC_RALT,          _______, KC_LCTL, KC_RGHT \
+    KC_LCTL, KC_LCTL, _______,          KC_LALT, NAV,     KC_SPC ,         KC_ENT,  SYM,     KC_RALT,          _______, KC_LCTL, FLIP     \
+  ),
+
+  [_FLIP] = LAYOUT(
+    RESET,   _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______,          _______, SYM,     _______,         _______, NAV,     _______,          _______, _______, _______ \
   ),
 
   [_NAV] = LAYOUT(
