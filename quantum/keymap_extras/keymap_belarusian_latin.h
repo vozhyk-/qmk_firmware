adcdd5e5c6746d06b44a7cda59d946116551edd6
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2020-07-06 00:08:58 +0200

    Add a Belarusian Latin layer

---
+++ quantum/keymap_extras/keymap_belarusian_latin.h
@@ -0,0 +1,39 @@
+/* Copyright 2020
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "keymap.h"
+
+// clang-format off
+
+// TODO: Make this look like the other keymaps.
+
+// Row 2
+#define BY_SCAR ALGR(KC_W)  // Š
+#define BY_UBRV ALGR(KC_U)  // Ŭ
+
+// Row 3
+#define BY_SACU ALGR(KC_S)  // Ś
+#define BY_LSTR ALGR(KC_L)  // Ł
+
+// Row 4
+#define BY_ZACU ALGR(KC_Z)  // Ź
+#define BY_ZCAR ALGR(KC_X)  // Ž
+#define BY_CACU ALGR(KC_C)  // Ć
+#define BY_CCAR ALGR(KC_V)  // Č
+#define BY_NACU ALGR(KC_N)  // Ń
+
